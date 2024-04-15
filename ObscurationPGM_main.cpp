f7e3bd3e2cb4457d8d806e3fdbbd7259b767ecc9
K. Auxilien <125131297+KatiaAuxilien@users.noreply.github.com>
2024-04-15 13:01:47 +0200

    <feat> Chiffrement AES mode ECB mis en place.

--- ObscurationPGM_main.cpp
+++ ObscurationPGM_main.cpp
@@ -3,6 +3,11 @@
 #include "include/obscurationPGM.h"
 #include "include/filesystemPGM.h"
 #include "include/filesystemCommon.h"
+#include "include/AES.h"
+extern "C"
+{
+#include "include/image_ppm.h"
+}
 
 #include <cstdio>
 #include <cstring>
@@ -28,20 +33,23 @@ int main(int argc, char **argv)
     std::string sImgClass = getLastDirectoryName(sFolderPath);
     std::string sNewFolderPath;
     std::vector<std::string> v_sImagePaths;
-
+/*
     // ============================ DEBUT Blurring ============================ //
-    std::cout << "Debut floutage moyenneur..." << "\n";
+    std::cout << "Debut floutage moyenneur..."
+              << "\n";
 
     std::strcpy(t_cImagePath, sFolderPath.c_str());
     sNewFolderPath = getProgramFolderPath(argv[0]) + "/obscuredPPM/blurring/" + sImgClass;
     getFilePathsOfPGMFilesFromFolder(v_sImagePaths, t_cImagePath);
 
-    for (int nImg_cpt = 0; nImg_cpt < v_sImagePaths.size(); ++nImg_cpt) {
+    for (int nImg_cpt = 0; nImg_cpt < v_sImagePaths.size(); ++nImg_cpt)
+    {
         ImageBase ibImgInOriginal;
-        std::string sImInPath = sFolderPath + '/' + sImgClass + "_ (" + std::to_string(nImg_cpt+1) + ")" + FILE_EXT;
+        std::string sImInPath = sFolderPath + '/' + sImgClass + "_ (" + std::to_string(nImg_cpt + 1) + ")" + FILE_EXT;
         std::strcpy(t_cImagePath, sImInPath.c_str());
         ibImgInOriginal.load(t_cImagePath);
-        std::cout << "Image n°" << nImg_cpt+1 << " en cours ... " <<"\n";
+        std::cout << "Image n°" << nImg_cpt + 1 << " en cours ... "
+                  << "\n";
 
         std::string sNewImgPath = sNewFolderPath + "/" + std::to_string(nImg_cpt);
         createDirectoryIfNotExists(sNewImgPath);
@@ -52,30 +60,34 @@ int main(int argc, char **argv)
         std::vector<ImageBase> v_ibImgOut;
         newAverageBlurring_PGM(ibImgInResized, v_ibImgOut);
 
-        for (int cpt = 1; cpt <= 101; cpt+=2) {
-            std::string sImOutPath = sNewImgPath + "/" + sImgClass
-                                    + "_" + std::to_string(nImg_cpt) + "_" + std::to_string(cpt) + FILE_EXT;
+        for (int cpt = 1; cpt <= 101; cpt += 2)
+        {
+            std::string sImOutPath = sNewImgPath + "/" + sImgClass + "_" + std::to_string(nImg_cpt) + "_" + std::to_string(cpt) + FILE_EXT;
             std::strcpy(t_cImagePath, sImOutPath.c_str());
-            v_ibImgOut[cpt/2].save(t_cImagePath);
+            v_ibImgOut[cpt / 2].save(t_cImagePath);
         }
-        std::cout << "Image " << nImg_cpt+1 << " sur " << v_sImagePaths.size() << "\n";
+        std::cout << "Image " << nImg_cpt + 1 << " sur " << v_sImagePaths.size() << "\n";
     }
-    std::cout << "Fin floutage moyenneur" << "\n";
+    std::cout << "Fin floutage moyenneur"
+              << "\n";
     // ============================ FIN Blurring ============================ //
 
     // ============================ DEBUT Scrambling ============================ //
-    std::cout << "Debut melange par region..." << "\n";
+    std::cout << "Debut melange par region..."
+              << "\n";
 
     std::strcpy(t_cImagePath, sFolderPath.c_str());
     sNewFolderPath = getProgramFolderPath(argv[0]) + "/obscuredPPM/scrambling/" + sImgClass;
     getFilePathsOfPGMFilesFromFolder(v_sImagePaths, t_cImagePath);
 
-    for (int nImg_cpt = 0; nImg_cpt < v_sImagePaths.size(); ++nImg_cpt) {
+    for (int nImg_cpt = 0; nImg_cpt < v_sImagePaths.size(); ++nImg_cpt)
+    {
         ImageBase ibImgInOriginal;
-        std::string sImInPath = sFolderPath + '/' + sImgClass + "_ (" + std::to_string(nImg_cpt+1) + ")" + FILE_EXT;
+        std::string sImInPath = sFolderPath + '/' + sImgClass + "_ (" + std::to_string(nImg_cpt + 1) + ")" + FILE_EXT;
         std::strcpy(t_cImagePath, sImInPath.c_str());
         ibImgInOriginal.load(t_cImagePath);
-        std::cout << "Image n°" << nImg_cpt+1 << " en cours ... " <<"\n";
+        std::cout << "Image n°" << nImg_cpt + 1 << " en cours ... "
+                  << "\n";
 
         std::string sNewImgPath = sNewFolderPath + "/" + std::to_string(nImg_cpt);
         createDirectoryIfNotExists(sNewImgPath);
@@ -83,34 +95,38 @@ int main(int argc, char **argv)
         ImageBase ibImgInResized(299, 299, false);
         bilinearRedim299_PGM(ibImgInOriginal, ibImgInResized);
 
-        for (int cpt = 1; cpt <= 50; ++cpt) {
-            ImageBase ibImgOut(ibImgInResized.getWidth(),ibImgInResized.getHeight(),ibImgInResized.getColor());
+        for (int cpt = 1; cpt <= 50; ++cpt)
+        {
+            ImageBase ibImgOut(ibImgInResized.getWidth(), ibImgInResized.getHeight(), ibImgInResized.getColor());
 
-            std::string sImOutPath = sNewImgPath + "/" + sImgClass
-                                    + "_" + std::to_string(nImg_cpt) + "_" + std::to_string(cpt) + FILE_EXT;
+            std::string sImOutPath = sNewImgPath + "/" + sImgClass + "_" + std::to_string(nImg_cpt) + "_" + std::to_string(cpt) + FILE_EXT;
             std::strcpy(t_cImagePath, sImOutPath.c_str());
 
-            scrambling_PGM(ibImgInResized,ibImgOut,cpt,cpt);
+            scrambling_PGM(ibImgInResized, ibImgOut, cpt, cpt);
             ibImgOut.save(t_cImagePath);
         }
-        std::cout << "Image " << nImg_cpt+1 << " sur " << v_sImagePaths.size() << "\n";
+        std::cout << "Image " << nImg_cpt + 1 << " sur " << v_sImagePaths.size() << "\n";
     }
-    std::cout << "Fin melange par region" << "\n";
+    std::cout << "Fin melange par region"
+              << "\n";
     // ============================ FIN Scrambling ============================ //
 
     // ============================ DEBUT Pixelisation ============================ //
-    std::cout << "Debut moyenneur par r�gion..." << "\n";
+    std::cout << "Debut moyenneur par r�gion..."
+              << "\n";
 
     std::strcpy(t_cImagePath, sFolderPath.c_str());
     sNewFolderPath = getProgramFolderPath(argv[0]) + "/obscuredPPM/pixeliser/" + sImgClass;
     getFilePathsOfPGMFilesFromFolder(v_sImagePaths, t_cImagePath);
 
-    for (int nImg_cpt = 0; nImg_cpt < v_sImagePaths.size(); ++nImg_cpt) {
+    for (int nImg_cpt = 0; nImg_cpt < v_sImagePaths.size(); ++nImg_cpt)
+    {
         ImageBase ibImgInOriginal;
-        std::string sImInPath = sFolderPath + '/' + sImgClass + "_ (" + std::to_string(nImg_cpt+1) + ")" + FILE_EXT;
+        std::string sImInPath = sFolderPath + '/' + sImgClass + "_ (" + std::to_string(nImg_cpt + 1) + ")" + FILE_EXT;
         std::strcpy(t_cImagePath, sImInPath.c_str());
         ibImgInOriginal.load(t_cImagePath);
-        std::cout << "Image n°" << nImg_cpt+1 << " en cours ... " <<"\n";
+        std::cout << "Image n°" << nImg_cpt + 1 << " en cours ... "
+                  << "\n";
 
         std::string sNewImgPath = sNewFolderPath + "/" + std::to_string(nImg_cpt);
         createDirectoryIfNotExists(sNewImgPath);
@@ -118,22 +134,26 @@ int main(int argc, char **argv)
         ImageBase ibImgInResized(299, 299, false);
         bilinearRedim299_PGM(ibImgInOriginal, ibImgInResized);
 
-        for (int cpt = 1; cpt <= 50; ++cpt) {
-            ImageBase ibImgOut(ibImgInResized.getWidth(),ibImgInResized.getHeight(),ibImgInResized.getColor());
+        for (int cpt = 1; cpt <= 50; ++cpt)
+        {
+            ImageBase ibImgOut(ibImgInResized.getWidth(), ibImgInResized.getHeight(), ibImgInResized.getColor());
 
-            std::string sImOutPath = sNewImgPath + "/" + sImgClass
-                                    + "_" + std::to_string(nImg_cpt) + "_" + std::to_string(cpt) + FILE_EXT;
+            std::string sImOutPath = sNewImgPath + "/" + sImgClass + "_" + std::to_string(nImg_cpt) + "_" + std::to_string(cpt) + FILE_EXT;
             std::strcpy(t_cImagePath, sImOutPath.c_str());
 
-            averageByRegion_PGM(ibImgInResized,ibImgOut,cpt,cpt);
+            averageByRegion_PGM(ibImgInResized, ibImgOut, cpt, cpt);
             ibImgOut.save(t_cImagePath);
         }
-        std::cout << "Image " << nImg_cpt+1 << " sur " << v_sImagePaths.size() << "\n";
+        std::cout << "Image " << nImg_cpt + 1 << " sur " << v_sImagePaths.size() << "\n";
     }
-    std::cout << "Fin moyenneur par region" << "\n";
+    std::cout << "Fin moyenneur par region"
+              << "\n";
     // ============================ FIN Pixelisation ============================ //
 
     // ============================ DEBUT Chiffrement sélectif ============================ //
+    std::cout << "\t"
+              << "Chiffrement sélectif"
+              << "\n";
 
     // ============== DEBUT Chiffrement sélectif par bit individuel ============== //
 
@@ -301,7 +321,8 @@ int main(int argc, char **argv)
         {
             std::vector<std::vector<int>> v_nBitGroup;
             genBitsGroups(v_nBitGroup, nGSize);
-            for (int i = 0; i < v_nBitGroup.size(); ++i) {
+            for (int i = 0; i < v_nBitGroup.size(); ++i)
+            {
                 int nGroup[8];
                 intVec2intArray(v_nBitGroup[i], nGroup);
                 ImageBase ibImgOut(ibImgInResized.getWidth(), ibImgInResized.getHeight(), ibImgInResized.getColor());
@@ -319,10 +340,60 @@ int main(int argc, char **argv)
               << "\n";
     // ============== FIN Chiffrement sélectif par bit groupé============== //
     // ============================ FIN Chiffrement sélectif ============================ //
-
-
+*/
     // ============================ DEBUT Chiffrement AES ============================ //
+    std::cout << "\t"
+              << "Chiffrement AES"
+              << "\n";
+
+    char t_cImageInPath[200], t_cImageOutPath[200];
+
     // ============== DEBUT Chiffrement AES ECB ============== //
+
+    std::cout << "Debut chiffrement AES mode ECB..."
+              << "\n";
+    std::strcpy(t_cImagePath, sFolderPath.c_str());
+    sNewFolderPath = getProgramFolderPath(argv[0]) + "/obscuredPPM/encryption/AES/ECB/" + sImgClass;
+    getFilePathsOfPGMFilesFromFolder(v_sImagePaths, t_cImagePath);
+
+    for (int nImg_cpt = 0; nImg_cpt < v_sImagePaths.size(); ++nImg_cpt)
+    {
+        std::string sImInPath = sFolderPath + '/' + sImgClass + "_ (" + std::to_string(nImg_cpt + 1) + ")" + FILE_EXT;
+        std::strcpy(t_cImageInPath, sImInPath.c_str());
+
+        std::cout << "Image n°" << nImg_cpt + 1 << " en cours ... "
+                  << "\n";
+
+        std::string sNewImgPath = sNewFolderPath + "/" + std::to_string(nImg_cpt);
+        createDirectoryIfNotExists(sNewImgPath);
+
+        std::string sImOutPath = sNewImgPath + "/" + sImgClass + "_" + std::to_string(nImg_cpt) + FILE_EXT;
+        std::strcpy(t_cImageOutPath, sImOutPath.c_str());
+
+        int nH, nW, nTaille;
+        OCTET *oImgIn, *oImgOut;
+
+        lire_nb_lignes_colonnes_image_pgm(t_cImageInPath, &nH, &nW);
+        nTaille = nH * nW;
+
+        allocation_tableau(oImgIn, OCTET, nTaille);
+        lire_image_pgm(t_cImageInPath, oImgIn, nH * nW);
+        allocation_tableau(oImgOut, OCTET, nTaille);
+
+        AES aes(AESKeyLength::AES_128);
+
+        unsigned char key[] = {0x96, 0x39, 0xb4, 0xfa, 0xe6, 0x52, 0xd1, 0x84, 0x59, 0x97, 0x3b, 0xd9, 0x26, 0xde, 0x71, 0x5b};
+
+        oImgOut = aes.EncryptECB(oImgIn, nTaille, key);
+
+        ecrire_image_pgm(t_cImageOutPath, oImgOut, nH, nW);
+        free(oImgIn);
+
+        std::cout << "Image " << nImg_cpt + 1 << " sur " << v_sImagePaths.size() << "\n";
+    }
+    std::cout << "Fin chiffrement AES mode ECB"
+              << "\n";
+
     // ============== FIN Chiffrement AES ECB ============== //
 
     // ============== DEBUT Chiffrement AES CBC ============== //
